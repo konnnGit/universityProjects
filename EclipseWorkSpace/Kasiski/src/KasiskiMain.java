6f0c9143ca6386b673a5eb54a608207bb9fd52f4
Konstantinos Spalas <konnn.sp@gmail.com>
2014-11-27 12:48:04 +0200

    Latest

--- EclipseWorkSpace/Kasiski/src/KasiskiMain.java
+++ EclipseWorkSpace/Kasiski/src/KasiskiMain.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class KasiskiMain {
 	public static void main(String[] args) {
+		
 	motivesList	list=new motivesList();
-	LinkedList<Motive> l=new LinkedList<Motive>();
-	Motive n=null;
-//	Iterator<Motive> iterList =list.iterator();
-	//motivesList<Motive> iterList2 =new motivesList<Motive>();
+	LinkedList<String> l=new LinkedList<String>();
+	
 	String input =null;
 	Scanner fileScanner =null;
     Scanner terminalScanner = new Scanner(System.in);
     int j,i,up,down;
     j=i=up=down=0;
+    up=down+3;
     String file="";
     String scanner=null;
     String s123;
@@ -35,51 +35,83 @@ public class KasiskiMain {
 	//System.out.print(file);
 	file=file.replaceAll("\\W+", "");//remove all no-letters
 	file=file.toUpperCase();
-	System.out.print(file);
+	//System.out.print(file);
 	char[] s=new char[file.length()];
 	s=file.toCharArray();
-	String[] strTrimmed=new String[file.length()];
-	/*
-	while(i<file.length()/3 && up+3<file.length()){	        
-    	strTrimmed[i]=file.substring(down,up);
-    	System.out.print(strTrimmed[i]+",");
-    	down=down+3;
-    	up=up+3;
-    	i++;	        	
-    }
 	
-	for(j=0;j<i;j++){
-		list.addToStart(strTrimmed[j], j);
-		System.out.print(list.getNewEntry()+",");
-	}*/
-	i++;
+	
 	System.out.println();
-	while( up+3<file.length()){	 
-		
-		n.newMotive(file.substring(down,up),i);
-		l.add(i,n );
-    	System.out.print(l.get(i).getMotiveName()+",");
+	while( up+3<file.length()&&i<file.length()/3){	
+		//s123=file.substring(down,up);
+		//System.out.print(s123+'-');
+		l.add(file.substring(down,up) );    	
     	down=down+3;
     	up=up+3;
     	i++;	        	
     }
 	
-
+	Set<String> uniqueSet = new HashSet<String>(l);
+	List<Integer> indexes = new ArrayList<Integer>();
 	
-	while(list.iterator().hasNext()){
-		System.out.print(list.iterator().next().getMotiveName()+",");
-	}
-/*	
-	while(iterList.hasNext()){
-		
-		if(iterList.equals(iterList.next())){
-			System.out.print(iterList.next().getMotiveName()+":");
-			System.out.print(iterList.next().getPosition());
-		}
+	/*--Frequency of motives---*/
+	for (String temp : uniqueSet) {
+		System.out.println(temp + " frequency: " + Collections.frequency(l, temp)+",");
+		System.out.print(l.indexOf(temp));
 		System.out.println();
 	}
-	*/
-    
+	/*--!Frequency of motives---*/
+	
+/*	while (l.iterator().hasNext()){
+		String tmp1=l.iterator().next();
+		while(l.iterator().hasNext()){
+			String tmp2=l.iterator().next();
+			int in=l.indexOf(l.iterator().next());
+			if (tmp1.equals(tmp2)) indexes.add(in);
+		}
+		System.out.println("indexes"+tmp1 +":"+indexes);
+		indexes.clear();
+	}*/
+	/*for (String temp1 : l) {
+		for (String temp2 : l) {
+			if (temp1.equals(temp2)){
+				indexes.add(l.indexOf(temp1));
+				indexes.add(l.indexOf(temp2));
+			}
+		}
+		System.out.println("indexes"+temp1 +":"+indexes);
+		indexes.clear();
+	}*/
+	
+	for(i=0;i<l.size()-1;i++){
+		indexes.add(l.indexOf(l.get(i)));
+		for(j=1;j<l.size();j++){
+			if(l.get(i).equals(l.get(j))) indexes.add(l.indexOf(l.get(j)));
+		}
+		System.out.println("indexes"+l.get(i) +":"+indexes);
+		indexes.clear();
+	}
+	
+	/*
+	for(i=0;i<l.size();i++){
+		if(l.iterator().equals(l.get(i))){
+			indexes.add(l.indexOf(l.get(i)));
+			//k=l.get(i).indexOf(l.get(i));
+			System.out.println(l.indexOf(l.get(i)));
+		}
+		System.out.println("indexes"+l.get(i) +":"+indexes);
+		indexes.clear();
+	}*/
+	
+	
+	//System.out.println(indexes);
+	/*System.out.println("Name of the most motive : ");
+	input=terminalScanner.nextLine();
+    terminalScanner.close();
+	for (String temp=input : uniqueSet) {
+		
+		
+	}*/
+	
 	
 
 }
