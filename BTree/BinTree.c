5c8d65d17b6a46a91716650b8c4e9d336b135af6
Konstantinos Spalas <konnn.sp@gmail.com>
2014-12-04 19:14:33 +0200

    Latest

---
+++ BTree/BinTree.c
@@ -0,0 +1,18 @@
+/*-----Konstantinos Spalas , AM: 2025200800094---------*/
+
+#include <stdio.h>
+
+typedef struct node{
+	int key;
+	struct node *left;
+	struct node *right;
+}node;
+	
+
+int main(int argc, char **argv){
+	
+	
+	
+	
+	return 1;
+}
